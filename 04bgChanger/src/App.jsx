f9f4b68cbcc4e02f22b3862c84b807dfc9c8b00a
Hanumant-Shinde-777 <hanumantshinde1213@gmail.com>
2023-11-01 21:39:33 +0530

    Changed Button Color line

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 <div className="w-full h-screen duration-200"
    style={{backgroundColor:color}} >
     <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
-<div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl"> 
+<div className="flex flex-wrap justify-center gap-3 shadow-lg bg-Baby to-pink-400 px-3 py-2 rounded-3xl"> 
 {/* Botton for the White color */}
 <button 
 //style={{background:"Dark Slate Gray"}}

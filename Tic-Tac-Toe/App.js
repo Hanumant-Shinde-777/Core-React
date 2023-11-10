6cf5832902390e334585d4feb42d54f9d1c19cb9
Hanumant-Shinde-777 <hanumantshinde1213@gmail.com>
2023-11-10 23:17:14 +0530

    logic for winner

--- Tic-Tac-Toe/App.js
+++ Tic-Tac-Toe/App.js
@@ -7,3 +7,18 @@ function Square({ value, onSquareClick }) {
     );
   }
   
+  //Function for Board
+function Board({ xIsNext, squares, onPlay }) {
+    function handleClick(i) {
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+      const nextSquares = squares.slice();
+      if (xIsNext) {
+        nextSquares[i] = 'X';
+      } else {
+        nextSquares[i] = 'O';
+      }
+      onPlay(nextSquares);
+    }
+  
